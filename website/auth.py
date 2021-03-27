1c1d3c1948af5d56a776d5cf6d5ea96228b11f30
DG <gchakradhar72@gmail.com>
2021-03-28 00:44:21 +0530

    basic login

--- website/auth.py
+++ website/auth.py
@@ -1,11 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/')
 def signIn():
-	return '<h1>signin</h1>'
+	return render_template('signin.html')
 
 @auth.route('/signup')
 def signUp():
