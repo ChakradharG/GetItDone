bf5f351471664485383266ca3fa65e4e6a3e9454
DG <gchakradhar72@gmail.com>
2021-03-28 00:59:56 +0530

    read and send .todo

--- website/views.py
+++ website/views.py
@@ -1,8 +1,17 @@
 from flask import Blueprint, render_template
+import os
 
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+def landing():
+	return '<script>window.location.href="/auth"</script>'
+
+@views.route('/home')
 def home():
-	return render_template('base.html')
+	s = ''
+	with (open('.todo', 'r')) as file:
+		for i in file.readlines():
+			s += i.replace('<', '&lt;')
+	return f'<pre>{s}</pre>'
