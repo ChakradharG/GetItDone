3b3f90134e93ca415ef4568f79c25e2d133cc62f
DG <gchakradhar72@gmail.com>
2021-03-29 19:54:09 +0530

    added api endpoint for tasks and .todo parsing

--- website/views.py
+++ website/views.py
@@ -1,8 +1,26 @@
 from flask import Blueprint, render_template
 import os
+import re
+import json
 
 
 views = Blueprint('views', __name__)
+regExOb = re.compile(r'\[(x?)\] <(.*)> (.*)')
+
+def parseAsTask(taskOb):
+	task = {'done': False, 'class': taskOb.group(2), 'cont': taskOb.group(3)}
+	if (taskOb.group(1) == 'x'): task['done'] = True
+	return task
+
+def tasksToJSON():
+	tasksList = []
+	# if todo: 
+	with (open('.todo', 'r')) as file:
+		for i in file.readlines():
+			if (taskOb := regExOb.match(i)) != None:
+				tasksList.append(parseAsTask(taskOb))
+	return json.dumps(tasksList)
+				
 
 @views.route('/')
 def landing():
@@ -10,8 +28,8 @@ def landing():
 
 @views.route('/home')
 def home():
-	s = ''
-	with (open('.todo', 'r')) as file:
-		for i in file.readlines():
-			s += i.replace('<', '&lt;')
-	return f'<pre>{s}</pre>'
+	return render_template('home.html')
+
+@views.route('/api')
+def getTasks():
+	return tasksToJSON()
