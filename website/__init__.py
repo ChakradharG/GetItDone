e4683019c6fc70fa344cc7307c8a16d612a3e3fb
DG <gchakradhar72@gmail.com>
2021-04-01 12:49:52 +0530

    Moved secret_key to secret.json and refactored a bit

--- website/__init__.py
+++ website/__init__.py
@@ -1,15 +1,15 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+import os
 import json
 
-from flask import Flask
-from dotenv import load_dotenv
-import os
 
-from flask_pymongo import PyMongo
-
-load_dotenv()
+with open('secret.json') as file:
+	data = json.loads(file.read())
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = data['SECRET_KEY']
+app.config['MONGO_URI'] = data['MONGO_URI']
 
 from .views import views
 from .auth import auth
@@ -17,8 +17,4 @@ from .auth import auth
 app.register_blueprint(views, url_prefix='/')
 app.register_blueprint(auth, url_prefix='/auth')
 
-
-with open("./website/static/secret.json") as file:
-    data = json.loads(file.read())
-app.config['MONGO_URI'] = data['mongo_uri']
 mongo = PyMongo(app)
