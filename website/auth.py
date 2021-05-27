33269d904856c5f318591a72e323e42d61d85121
DG <gchakradhar72@gmail.com>
2021-05-27 12:12:12 +0530

    merged security and auth

--- website/auth.py
+++ website/auth.py
@@ -1,14 +1,25 @@
 from flask import Blueprint, render_template, request
+from passlib.context import CryptContext
 
 
 auth = Blueprint('auth', __name__)
+pwd_context = CryptContext(
+	schemes=["pbkdf2_sha256"],
+	default="pbkdf2_sha256",
+	pbkdf2_sha256__default_rounds = 30000)
+
+def encrypt_password(password):
+	return pwd_context.encrypt(password)
+
+def check_password(password, hashed_value):
+	return pwd_context.verify(password, hashed_value)
 
 @auth.route('/', methods=['GET', 'POST'])
 def signIn():
 	if request.method == 'GET':
 		return render_template('signin.html')
 	elif request.method == 'POST':
-		data = dict(request.form)
+		# data = dict(request.form)
 		return render_template('home.html')
 
 
@@ -17,5 +28,5 @@ def signUp():
 	if request.method == 'GET':
 		return render_template('signup.html')
 	elif request.method == 'POST':
-		data = dict(request.form)
+		# data = dict(request.form)
 		return render_template('signin.html')
