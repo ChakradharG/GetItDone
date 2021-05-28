6464af7f882fd3a631d948e96731b21b7cae61eb
DG <gchakradhar72@gmail.com>
2021-05-28 11:18:47 +0530

    addtask done (frontend)

--- website/static/JS/index.js
+++ website/static/JS/index.js
@@ -1,3 +1,4 @@
+const tdl = document.getElementById('to-do-list');
 let tasks = null;
 
 (async () => {
@@ -7,11 +8,33 @@ let tasks = null;
 })();
 
 function renderTasks() {
-	const tdl = document.getElementById('to-do-list');
 	for (let i of tasks) {
 		let task = document.createElement('div');
 		task.className = 'task-card';
-		task.innerHTML = `<p>${i.cont}<p style="color:gray;">${i.class}</p></p>`;
+		task.innerHTML = `<p>${i.cont}</p><p style="color:gray;">${i.class}</p>`;
 		tdl.append(task);
 	}
+}
+
+function addTask() {
+	let task = document.createElement('div');
+	task.className = 'task-card';
+	let inp = document.createElement('input');
+	inp.className = 'enter-task';
+	let inp2 = document.createElement('input');
+	inp2.className = 'enter-task-class';
+	inp.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			inp2.focus();
+		}
+	});
+	inp2.addEventListener('keydown', (event) => {
+		if (event.key === 'Enter') {
+			task.innerHTML = `<p>${inp.value}</p><p style="color:gray;">${inp2.value}</p>`;
+		}
+	});
+	task.appendChild(inp);
+	task.appendChild(inp2);
+	tdl.append(task);
+	inp.focus();
 }
