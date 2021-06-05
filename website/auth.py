2be9d50979b6b77582d49eb03c23d58741935732
DG <gchakradhar72@gmail.com>
2021-06-05 15:38:21 +0530

    signin logic

--- website/auth.py
+++ website/auth.py
@@ -3,25 +3,31 @@ from passlib.context import CryptContext
 
 
 auth = Blueprint('auth', __name__)
-pwd_context = CryptContext(
+encryptor = CryptContext(
 	schemes=["pbkdf2_sha256"],
 	default="pbkdf2_sha256",
 	pbkdf2_sha256__default_rounds = 30000)
 
-def encrypt_password(password):
-	return pwd_context.encrypt(password)
+def initAuthDB(dbObject):
+	global DB
+	DB = dbObject
 
-def check_password(password, hashed_value):
-	return pwd_context.verify(password, hashed_value)
+# encryptor.hash(password)
 
 @auth.route('/', methods=['GET', 'POST'])
 def signIn():
 	if request.method == 'GET':
 		return render_template('signin.html')
 	elif request.method == 'POST':
-		# formData = dict(request.form)
-		# check_password
-		return '<script>window.location.replace("/home")</script>'
+		formData = dict(request.form)
+		isValid = encryptor.verify(
+			formData['password'],
+			DB.getUserCredentials(formData['username'])['pwdHash']
+		)
+		if isValid:
+			return f'<script>localStorage.setItem("username", "{formData["username"]}"); window.location.replace("/home")</script>'
+		else:
+			return render_template('signin.html', message='Invalid credentials, please try again\n')
 
 @auth.route('/signup', methods=['GET', 'POST'])
 def signUp():
