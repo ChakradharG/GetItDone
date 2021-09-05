33fb8f64ce1f61ba0597b032d74740d52cfc27de
harsh2799 <mishraharsh27@gmail.com>
2021-09-05 18:03:17 +0530

    changes in TODO and CSS

--- website/auth.py
+++ website/auth.py
@@ -18,7 +18,7 @@ def initAuthDB(dbObject):
 @auth.route('/', methods=['GET', 'POST'])
 def logIn():
 	if request.method == 'GET':
-		return render_template('todo.html')
+		return render_template('login.html')
 	elif request.method == 'POST':
 		form = request.get_json()
 		try:
